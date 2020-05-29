2abe2ee7033932913f2032325be0a411b5e19686
98savage <nikita-anoshin@yandex.ru>
2020-05-29 18:20:06 +0300

    add change image on hover

--- netclicks/js/main.js
+++ netclicks/js/main.js
@@ -1,5 +1,7 @@
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
+const tvShowList = document.querySelector('.tv-shows__list');
+const modal = document.querySelector('.modal');
 
 hamburger.addEventListener('click', () => {
 	leftMenu.classList.toggle('openMenu');
@@ -23,3 +25,43 @@ leftMenu.addEventListener('click', event => {
 		hamburger.classList.add('open');
 	}
 });
+
+tvShowList.addEventListener('click', event => {
+
+	event.preventDefault();
+
+	const target = event.target;
+	const card = target.closest('.tv-card');
+
+	if (card) {
+		document.body.style.overflow = 'hidden';
+		modal.classList.remove('hide');
+
+	}
+});
+
+modal.addEventListener('click', event => {
+	const target = event.target;
+	const cross = target.closest('.cross');
+
+	if(cross || 
+		event.target.classList.contains('modal')) {
+		document.body.style.overflow = '';
+		modal.classList.add('hide');
+	}
+});
+
+const changeImage = event => {
+	const card = event.target.closest('.tv-shows__item'); 
+
+	if (card) {
+		const img = card.querySelector('.tv-card__img');
+		if (img.dataset.backdrop) {
+			[img.src, img.dataset.backdrop] = [img.dataset.backdrop, img.src]
+		}
+		
+	}
+};
+
+tvShowList.addEventListener('mouseover', changeImage);
+tvShowList.addEventListener('mouseout', changeImage);
