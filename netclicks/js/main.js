d33670449f9a18f538fdd5a0815ff6de4913ae84
98savage <nikita-anoshin@yandex.ru>
2020-05-29 23:06:19 +0300

    add modal window, change posters and titles, realize image for empty posters

--- netclicks/js/main.js
+++ netclicks/js/main.js
@@ -1,7 +1,63 @@
+const IMG_URL = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2';
+
+
 const leftMenu = document.querySelector('.left-menu');
 const hamburger = document.querySelector('.hamburger');
-const tvShowList = document.querySelector('.tv-shows__list');
+const tvShowsList = document.querySelector('.tv-shows__list');
 const modal = document.querySelector('.modal');
+
+
+class DBRequest {
+	getData = async (url) => {
+		const res = await fetch(url);
+		if (res.ok) {
+			return res.json();
+		} else {
+			throw new Error(`Не удалось получить данные по адресу ${url}`)
+		}
+	}
+
+	getTestData = () => {
+		return this.getData('test.json')
+	}
+}
+
+const renderCard = response => {
+	console.log(response);
+	tvShowsList.textContent = '';
+
+	response.results.forEach(item => {
+
+		const {
+			backdrop_path: backdrop,
+			name: title,
+			poster_path: poster,
+			vote_average: vote
+			} = item;
+
+			const posterIMG = poster ? IMG_URL + poster: 'img/no-poster.jpg';
+			const backdropIMG = '';
+			const voteElem = '';
+			
+			const card = document.createElement('li');
+			card.className = 'tv-shows__item';
+			card.innerHTML = `
+				<a href="#" class="tv-card">
+					<span class="tv-card__vote">${vote}</span>
+					<img class="tv-card__img"
+						src="${posterIMG}"
+						data-backdrop="${IMG_URL + backdrop}"
+						alt="${title}">
+					<h4 class="tv-card__head">${title}</h4>
+				</a>
+				`;
+
+				tvShowsList.append(card);
+		});
+	};
+
+new DBRequest().getTestData().then(renderCard);
+
 
 hamburger.addEventListener('click', () => {
 	leftMenu.classList.toggle('openMenu');
@@ -26,7 +82,7 @@ leftMenu.addEventListener('click', event => {
 	}
 });
 
-tvShowList.addEventListener('click', event => {
+tvShowsList.addEventListener('click', event => {
 
 	event.preventDefault();
 
@@ -63,5 +119,6 @@ const changeImage = event => {
 	}
 };
 
-tvShowList.addEventListener('mouseover', changeImage);
-tvShowList.addEventListener('mouseout', changeImage);
+tvShowsList.addEventListener('mouseover', changeImage);
+tvShowsList.addEventListener('mouseout', changeImage);
+
